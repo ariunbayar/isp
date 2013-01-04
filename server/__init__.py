2cd646f3d5e0cb8e5b1e5a30cf7123fc106d3080
bulga <mr.unagaldai@gmail.com>
2013-01-04 12:23:28 +0800

    R10: cron job to block server upon expiry

--- server/__init__.py
+++ server/__init__.py
@@ -1,14 +1,16 @@
 from forms import ServerForm, LoginInfoForm
 from server.models import Server, LoginInfo
 from account.models import Account
-from google.appengine.ext import ndb, db
-from google.appengine.api import mail
+from google.appengine.ext import ndb
+from google.appengine.api import mail, urlfetch
 from google.appengine.api.app_identity import get_application_id
 from flask import request, redirect, url_for, \
     render_template, Blueprint, abort, flash, make_response, current_app as app
 from helpers import get_server, get_login_info
 from decorator import check_login
 import datetime
+import time
+import urllib
 
 
 server_blueprint = Blueprint('server', __name__, template_folder='templates')
@@ -80,15 +82,39 @@ def expiry():
     sender = 'no-reply@' + get_application_id() + '.appspotmail.com'
     site_url = 'http://' + get_application_id() + '.appspot.com'
     tomorrow = datetime.datetime.combine(ub_today, last_min)
-    servers = Server.all().filter("expire_date <=", tomorrow)
-    accounts = Account.all()
+    servers = Server.query().filter(Server.expire_date<=tomorrow)
+    accounts = Account.query()
     for server in servers:
         for account in accounts:
             message = mail.EmailMessage(sender=sender, to=account.email)
             message.subject = "Server expire notification: %s" % server.server_name
-            message.body = "Server will expire at %s\n" \
-                            "%s%s" % (server.expire_date, site_url,
-                                    url_for('server.show', server_key=server.key()))
+            show_url = url_for('server.show', server_key=server.key.urlsafe())
+            message_body = "Server %s is expired at %s\n %s%s" \
+                           % (server.server_name, server.expire_date,
+                              site_url, show_url)
+
+            if server.blocked:
+                for i in range(3):
+                    url = 'http://%s/manager/expired/%s'\
+                          % (server.ip_address, server.token)
+                    result = urlfetch.fetch(url=url, deadline=10)
+
+                    if result.status_code == 200:
+                        break
+                    else:
+                        time.sleep(5)
+
+                if result.status_code == 200:
+                    if result.content == 'TRUE':
+                        message_body += '\n Server %s is blocked upon expiry, %s'\
+                                        % (server.server_name, server.expire_date)
+                    else:
+                        message_body += '\n %s' % result.content
+                else:
+                    message_body += '\n Unable to reach %s to block upon expiry, %s'\
+                                    % (server.server_name, server.expire_date)
+
+            message.body = message_body
             message.send()
 
     return make_response('hello')
